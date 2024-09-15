529e86a009124034a1efb5260f7a098e28224b12
Jaron Steenson <jaron.steenson@kitomba.com>
2024-09-16 09:54:58 +1200

    Try to find out what the earliest time the spec can be run

--- tests/book.spec.ts
+++ tests/book.spec.ts
@@ -1,41 +1,58 @@
 import { test } from '@playwright/test';
 
-
 /**
  * `0 12 * * 7` At 12:00 PM, only on Sunday.
  * Accounting for timezones and assuming we are running in utc, means 12am Monday night NZ time.
  */
 test('book court', async ({ page }) => {
-  // Login
-  await page.goto('/login/credentials');
-  await page.getByPlaceholder('Username').fill(process.env.USERNAME);
-  await page.getByPlaceholder('Password').fill(process.env.PASSWORD);
-  await page.locator('[type=submit]').click();
+  await login({ page });
 
-  // Wait for logged in app state and go to bookings page.
-  await page.locator('[href="/bookings"]').last().click();
+  await findBookingDay({ page });
 
-  // Cycle through days, till the next instance of our booking day is found.
-  let foundDay = false
-  while (!foundDay) {
-    await page.locator('.BookingGridNav button').last().click(); // Next day button.
-    foundDay = (await page.locator('.BookingGridNav-date').first().textContent()).includes(process.env.BOOKING_DAY)
-  }
-
-  // Book slots.
   await bookSlots({
-    page, startTime:
+    page,
+    startTime:
     process.env.BOOKING_TIME_START,
     endTime: process.env.BOOKING_TIME_END,
     uncheckConfirmationEmail: false
   });
   await bookSlots({
-    page, startTime:
+    page,
+    startTime:
     process.env.BOOKING_TIME_START_2,
     endTime: process.env.BOOKING_TIME_END_2,
     uncheckConfirmationEmail: true
   });
 });
+
+async function login({ page }) {
+  await page.goto('/login/credentials');
+  await page.getByPlaceholder('Username').fill(process.env.USERNAME);
+  await page.getByPlaceholder('Password').fill(process.env.PASSWORD);
+  await page.locator('[type=submit]').click();
+
+  await page.locator('[href="/bookings"]').last().click();
+}
+
+async function findBookingDay({ page }) {
+  // Cycle through days, till the next instance of our booking day is found.
+  let foundDay = false
+  let previousDate = ''
+  while (!foundDay) {
+    await page.locator('.BookingGridNav button').last().click(); // Next day button.
+    const currentDate = (await page.locator('.BookingGridNav-date').first().textContent());
+
+    if (currentDate.includes(process.env.BOOKING_DAY)) {
+      return;
+    }
+
+    if (previousDate === currentDate) {
+      throw new Error(`Unable to go to next instance of ${process.env.BOOKING_DAY}, spec probably run too early`);
+    }
+
+    previousDate = currentDate;
+  }
+}
 
 async function bookSlots({ page, startTime, endTime, uncheckConfirmationEmail }) {
   await page.getByText(startTime).nth(process.env.BOOKING_COURT - 1).click();
