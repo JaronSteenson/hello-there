9e11b3fa0c18525be843b6799b654b99f899cb0d
Jaron Steenson <jaron.steenson@kitomba.com>
2025-04-15 15:19:20 +1200

    Put your clocks back for the winter

---
+++ launch-chrome.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PATH=/home/jaron/.nvm/versions/node/v14.20.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin
+
+cd ~/code/hello-there;
+
+export CI=true;
+
+nvm use 18;
+npm run book;
